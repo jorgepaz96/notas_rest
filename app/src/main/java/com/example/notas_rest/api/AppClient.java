96b3ee25bd02c063ac898dec419436c614a8e188
jorge missael paz marcos <pazmissael@gmail.com>
2019-10-25 00:45:17 -0500

    ultimo cambio

--- app/src/main/java/com/example/notas_rest/api/AppClient.java
+++ app/src/main/java/com/example/notas_rest/api/AppClient.java
@@ -1,4 +1,4 @@
-package com.example.notas_rest;
+package com.example.notas_rest.api;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -7,6 +7,11 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class AppClient {
+//    NODE JS
+//    private static final String BASE_URL = "http://192.168.1.61:3000/";
+//    PYTHON
+//    private static final String BASE_URL = "http://192.168.1.61:5000/";
+//    PHP
     private static final String BASE_URL = "http://192.168.1.61/php_rest_note/";
     private static Retrofit retrofit;
 
